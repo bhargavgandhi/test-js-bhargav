955865da25f5bd17a0dab405c035441f9e742662
Bhargav Gandhi <bhargavbgandhi@gmail.com>
2017-06-19 15:49:59 -0400

    Add code to list sorted movies with styling update

--- src/components/movies-container.js
+++ src/components/movies-container.js
@@ -2,7 +2,24 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 export function MoviesContainer(props) {
-  return (<h1>TODO :)</h1>)
+  return (
+    <article className="movieLists">
+      {
+        props.movies.map(function(Object, i) {
+          return <figure className="movieListItem" key={i}>
+            <div className="movieImage">
+              <img src={Object.image} alt={Object.title} />
+            </div>
+            <div className="movieDetails">
+                  <h1>{Object.title}</h1>
+                  <h2>Price: ${Object.price}</h2>
+                  <p>Year: {Object.releaseYear}</p>
+            </div>
+          </figure>
+        })
+      }
+    </article>
+)
 }
 
 const mapStateToProps = state => {
--- src/state/actions.js
+++ src/state/actions.js
@@ -2,7 +2,7 @@ import moment from 'moment'
 import movies from './movies'
 
 
-export function getPopularMovies () {
+export function getPopularMovies() {
   //
   // movies contains the results of two API requests
   //
@@ -14,12 +14,43 @@ export function getPopularMovies () {
   //    this is used in src/components/movies-list.js line 25
   //
 
-  const combinedResults = []
+  const combinedResults = [];
+
+  var results = [];
+  results = movies.map(function(movie) {
+    return movie;
+  })
+
+  for (let i = 0; i < results.length; i++) {
+    for (let j = 0; j < results[i].length; j++) {
+      var releaseYear = new Date(results[i][j].releaseDate);
+      releaseYear = releaseYear.getFullYear();
+      results[i][j]["releaseYear"] = releaseYear;
+      combinedResults.push(results[i][j]);
+    }
+  }
+
+  combinedResults.sort(function(a, b) {
+
+    var aConcat = a["releaseYear"] + a["title"];
+    var bConcat = b["releaseYear"] + b["title"];
+
+    if (aConcat > bConcat) {
+      return 1;
+    } else if (aConcat < bConcat) {
+      return -1;
+    } else {
+      return 0;
+    }
+
+  });
+
+  console.log(combinedResults);
+
+
 
   return {
     type: 'GET_MOVIES_SUCCESS',
     movies: combinedResults
   }
 }
-
-
